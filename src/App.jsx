33f6cd29bb0771167dc6e36120d835690b61bc05
Shorno <roy15-6141@s.diu.edu.bd>
2024-10-24 23:19:14 +0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/navbar.jsx";
+import Hero from "./components/hero.jsx";
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
         <>
           <main className={"container mx-auto"}>
               <Navbar/>
+              <Hero/>
           </main>
         </>
     )
---
+++ src/components/hero.jsx
@@ -0,0 +1,25 @@
+import Banner from "../assets/banner-main.png"
+import BannerBackground from "../assets/bg-shadow.png"
+
+export default function Hero() {
+    return (
+        <div className="hero mt-10 py-8 bg-black md:rounded-2xl"
+            style={{
+                backgroundImage: `url(${BannerBackground})`,
+            }}>
+            <div className="hero-content text-neutral-content text-center">
+                <div className="flex flex-col max-w-6xl justify-center items-center space-y-8">
+                    <img className={"w-60"} src={Banner} alt="banner "/>
+                    <h1 className="mb-5 text-3xl md:text-5xl font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                    <p className="mb-5 text-white/75 text-xl md:text-2xl">
+                        Beyond Boundaries Beyond Limits
+                    </p>
+                    <div className={"border border-yellow-300 p-2 rounded-xl"}>
+                        <button className="btn btn-warning font-bold">Clim Free Credit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
